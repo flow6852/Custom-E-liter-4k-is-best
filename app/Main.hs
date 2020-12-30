2d3791bc7d1b8b8b24754fa00774e58b42255b88
flow6852 <flow6852math@gmail.com>
2020-12-30 19:28:35 +0900

    stack exec bin [user_id twitter_id] can reply

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Lib
 import System.Directory
 import qualified Data.Text.IO as TIO
 import qualified Data.Text as T
+import System.Environment
 
-api = ".config/Custom-E-liter-4k-is-best"
+api = "/.config/Custom-E-liter-4k-is-best"
 
 main :: IO ()
 main = getHomeDirectory >>= \home -> TIO.readFile (home ++ api) 
-                        >>= \bot -> tweet (T.pack "リッカスはいいぞ") (T.empty) ((Prelude.map T.unpack.T.lines) bot) >> return()
+                        >>= \bot -> getArgs
+                        >>= \args -> case args of
+                                        [] -> tweet "リッカスはいいぞ" (T.empty) ((Prelude.map T.unpack.T.lines) bot) >> return()
+                                        [user, id] -> tweet (foldl1 T.append [T.singleton '\n', T.pack user, "リッカスはいいぞ"]) (T.pack id) ((Prelude.map T.unpack.T.lines) bot) >> return()
+
